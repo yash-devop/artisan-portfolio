b6b246cdbf76d9ecbd2e88c444975fd748832938
yash-devop <yashkamble.dev@gmail.com>
2026-03-29 11:15:52 +0530

    [borders] updated

--- app/components/main-banner/main-banner.tsx
+++ app/components/main-banner/main-banner.tsx
@@ -1,14 +1,9 @@
-import { LinesWrapper } from "../LinesWrapper";
-
 export const MainBanner = () => {
   return (
-    <LinesWrapper
-      fullBorder="b"
-      className="h-56 w-full text-center flex items-center justify-center"
-    >
+    <div className="h-56 w-full text-center flex items-center justify-center border-b border-border">
       <span className="italic font-serif cursor-default select-none">
         Available to work
       </span>
-    </LinesWrapper>
+    </div>
   );
 };
--- app/components/profile-section/profile-section.tsx
+++ app/components/profile-section/profile-section.tsx
@@ -3,12 +3,9 @@ import { LinesWrapper } from "../LinesWrapper";
 import { IconRosetteDiscountCheck } from "@tabler/icons-react";
 export const ProfileSection = () => {
   return (
-    <LinesWrapper
-      fullBorder="b"
-      className="h-44 flex items-center justify-between"
-    >
+    <div className="h-44 flex flex-col md:flex-row items-center justify-between border-b border-border">
       <div className="w-full h-full flex items-end">
-        <LinesWrapper fullBorder="r" className="w-fit! shrink-0">
+        <div className="border-r border-border w-fit shrink-0">
           <Image
             src={
               "https://pbs.twimg.com/profile_images/1933830267388706816/DHs_-HrA_400x400.jpg"
@@ -17,33 +14,34 @@ export const ProfileSection = () => {
             width={175}
             height={175}
             objectFit="cover"
-            className="rounded-full shrink-0"
+            className="rounded-full shrink-0 w-[130px] h-[130px] md:w-[175px] md:h-[175px]"
           />
-        </LinesWrapper>
+        </div>
         <div className="w-full flex flex-col">
-          <LinesWrapper fullBorder="t" className="h-12 w-full px-4">
-            <div className="flex items-center justify-start w-full gap-2">
-              <span className="text-2xl font-medium tracking-tight text-start text-zinc-50">
-                Yash Kamble
-              </span>
-              <IconRosetteDiscountCheck
-                size={35}
-                className="text-black fill-blue-400"
-              />
-            </div>
-          </LinesWrapper>
-          <LinesWrapper fullBorder="t" className="h-8 w-full px-4">
-            <div className="flex items-center justify-start w-full gap-2">
-              <span className="text-neutral-500 text-sm">
-                Full Stack Engineer
-              </span>
-            </div>
-          </LinesWrapper>
+          <div className="h-8 w-full px-4 flex items-end justify-start gap-2 ">
+            <span className="text-sm font-medium text-start text-neutral-800 font-mono select-none pb-1">
+              text-2xl font-medium text-neutral-700
+            </span>
+          </div>
+          <div className="h-8 w-full px-4 flex items-center justify-start gap-2 border-y border-border">
+            <span className="text-2xl font-medium tracking-tight text-start text-zinc-50">
+              Yash Kamble
+            </span>
+            <IconRosetteDiscountCheck
+              size={35}
+              className="text-black fill-blue-400"
+            />
+          </div>
+          <div className="h-8 w-full px-4 flex items-center justify-start gap-2">
+            <span className="text-neutral-600 text-sm">
+              Full Stack Engineer
+            </span>
+          </div>
         </div>
       </div>
-      <LinesWrapper fullBorder="l" className="w-[500px]! h-full">
+      <div className="h-full border-y md:border-y-0 md:border-l border-border w-full md:w-[500px] flex items-center justify-center">
         <span>right</span>
-      </LinesWrapper>
-    </LinesWrapper>
+      </div>
+    </div>
   );
 };
