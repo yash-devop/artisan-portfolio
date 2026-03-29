dd7369712b445066f6842ceb073ae92eefb6168c
yash-devop <yashkamble.dev@gmail.com>
2026-03-29 11:15:15 +0530

    [grid-lines] keep only x y and x-y

--- app/components/LinesWrapper.tsx
+++ app/components/LinesWrapper.tsx
@@ -1,32 +1,21 @@
 import { cva } from "class-variance-authority";
 import { ComponentProps } from "react";
 
-type TLineVariants = "y" | "x" | "x-y" | "t" | "l" | "r" | "b";
-// type TLineVariants = "t" | "b" | "l" | "r" | "y" | "x" | "x-y";
+type TLineVariants = "y" | "x" | "x-y";
 const lineVariants = cva("relative flex items-center justify-center w-full", {
   variants: {
     fullBorder: {
-      t: "before:content-[''] before:absolute before:h-px before:w-full before:top-0 before:bg-border",
-      b: "w-full border-border flex items-center justify-center before:bg-border before:w-screen before:h-px before:content-[''] before:absolute before:bottom-0",
-      l: "before:content-[''] before:absolute before:w-px before:left-0 before:h-full before:bg-border",
-      r: " after:content-[''] after:absolute after:w-px after:right-0 after:h-full after:bg-border",
+      // t: "before:content-[''] before:absolute before:h-px before:w-full before:top-0 before:bg-border",
+      // b: "w-full border-border flex items-center justify-center before:bg-border before:w-screen before:h-px before:content-[''] before:absolute before:bottom-0",
+      // l: "before:content-[''] before:absolute before:w-px before:left-0 before:h-full before:bg-border",
+      // r: " after:content-[''] after:absolute after:w-px after:right-0 after:h-full after:bg-border",
       y: "w-full before:bg-border before:w-screen before:h-px before:content-[''] before:absolute before:top-0  after:content-[''] after:absolute after:w-screen after:h-px after:bottom-0 after:bg-border",
       x: "border-border before:bg-border before:h-full before:w-px before:content-[''] before:absolute before:left-0 after:content-[''] after:absolute after:w-px after:right-0 after:h-full after:bg-border",
       "x-y": "border border-border items-center justify-center",
     },
-    border: {
-      t: "border-t",
-      b: "border-b",
-      l: "border-l",
-      r: "border-r",
-      x: "border-x",
-      y: "border-y",
-      "x-y": "border",
-    },
   },
   defaultVariants: {
     fullBorder: "y",
-    // border: "x-y",
   },
 });
 export const LinesWrapper = ({
@@ -35,7 +24,6 @@ export const LinesWrapper = ({
   ...props
 }: ComponentProps<"div"> & {
   fullBorder?: TLineVariants;
-  border?: TLineVariants;
 }) => {
   return (
     <div className={lineVariants({ className, fullBorder })} {...props}></div>
