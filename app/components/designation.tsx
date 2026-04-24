e07f10128b721bf9cdf31081db43b9ed1c1116a3
yash-devop <yashkamble.dev@gmail.com>
2026-04-24 10:05:58 +0530

    [content] designation and some my content

--- app/components/designation.tsx
+++ app/components/designation.tsx
@@ -7,15 +7,19 @@ export const Designation = () => {
         <div className="size-10 rounded-full bg-linear-to-tr from-white border border-neutral-100 to-indigo-500 shrink-0" />
         <div className="flex flex-col leading-4  select-none">
           <div className="flex items-center gap-x-1.5">
-            <h2 className="text-lg font-medium">Yash Kamble</h2>
+            <h2 className="text-sm md:text-lg font-medium">Yash Kamble</h2>
             <IconRosetteDiscountCheckFilled className="fill-indigo-600 size-5" />
           </div>
-          <h2 className="text-neutral-500">Frontend Engineer</h2>
+          <h2 className="text-neutral-500 text-sm md:text-lg">
+            Frontend Engineer
+          </h2>
         </div>
       </div>
-      <div className="flex items-center gap-2 font-instrument-serif italic">
+      <div className="flex items-center gap-2 font-instrument-serif italic ">
         <div className="bg-emerald-600 size-2 mt-0.5 rounded-full" />
-        <span>Available for work</span>
+        <span className="font-medium text-sm md:text-lg">
+          Available for work
+        </span>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { IconBrandX, IconMail } from "@tabler/icons-react";
 import { DottedUnderline } from "./components/dotted-underline";
 
 export default function Home() {
@@ -6,25 +7,43 @@ export default function Home() {
       <div className="tracking-tight leading-[1.57] text-neutral-600">
         <p>
           A Frontend Developer based in India , who loves to give life to his
-          thoughts through his design and code.
+          thoughts through design and code.
         </p>
-        <div>
-          <span>I'm mostly active on </span>
-          <div className="relative w-fit inline">
-            <a
-              href=""
-              target="_blank"
-              className="text-neutral-400 font-instrument-serif italic font-light"
-            >
-              Twitter/x
-            </a>
+        <p className="">
+          I'm mostly active on
+          <a
+            href=""
+            target="_blank"
+            className="relative text-black font-medium px-1 space-x-0.5"
+          >
+            <IconBrandX size={16} className="shrink-0 inline-block mb-1" />
+            Twitter
             <DottedUnderline />
-          </div>
-          <span className="pl-1">
-            where i post mostly about my content and arts.
-          </span>
-        </div>
+          </a>
+          , where I post mostly about my content and arts.
+        </p>
+        <p className="mt-2">
+          You can reach out to me via{" "}
+          <a
+            href="mailto:your@email.com"
+            className="text-black font-medium pr-0.5 space-x-0.5 relative"
+          >
+            <IconMail size={18} className="shrink-0 inline-block mb-0.5" />
+            Email
+            <DottedUnderline />
+          </a>{" "}
+          or through
+          <a
+            href=""
+            target="_blank"
+            className="relative text-black font-medium px-1 inline-flex"
+          >
+            Linkedin.
+            <DottedUnderline />
+          </a>
+        </p>
       </div>
+      <div className="mt-4 border-y border-neutral-300 h-8 w-full bg-[repeating-linear-gradient(45deg,#afaeae_0px,#ffffff_1px,transparent_8px,transparent_7px)]" />
     </div>
   );
 }
